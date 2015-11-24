34cc863f224760a229362348b57d345b4e2107f6
Akihiro0222 <ak08222@yahoo.co.jp>
2015-11-24 17:07:57 +0900

    自機の移動と射撃　当たり判定は未作成

---
+++ Assets/Player/Script/KeyMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//右への移動
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 nowPos = this.transform.position;
+			nowPos.x = nowPos.x + 0.05f;
+			if(nowPos.x <2.0f)
+			this.transform.position=nowPos;
+		}
+
+		//左への移動
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector3 nowPos = this.transform.position;
+			nowPos.x = nowPos.x - 0.05f;
+			if(nowPos.x >-2.0f)
+			this.transform.position=nowPos;
+		}
+	}
+}
---
+++ Assets/Shot/ShotMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 nowPos = transform.position;
+
+		nowPos.z += 0.1f;
+
+		transform.position = nowPos;
+
+		if (transform.position.z > 8.0f) {
+			Destroy(gameObject);
+		}
+	}
+}
