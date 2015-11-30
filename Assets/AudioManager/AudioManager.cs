9b0a7117fdd6d1779c0592abe6da6708379bce72
Akihiro0222 <ak08222@yahoo.co.jp>
2015-11-30 09:07:25 +0900

    SoundManagerの追加

---
+++ Assets/AudioManager/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    //シングルトン用の変数
+    public static AudioManager instance = null;
+
+    //再生したいソースを確保する
+    [SerializeField]
+    private AudioClip exprotion = null;
+    [SerializeField]
+    private AudioClip shot = null;
+
+
+    //二種類の音を同時再生するために二つ用意する
+    [SerializeField]
+    private AudioSource audioSource_Shot;
+    [SerializeField]
+    private AudioSource audioSource_Exp;
+
+	// Use this for initialization
+	void Awake() {
+
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+            instance = this;
+        }
+        else if (instance == null)
+        {
+            instance = this;
+        }
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+
+    public void PlaySoundExprotion()
+    {
+        audioSource_Exp.clip = exprotion;
+
+        audioSource_Exp.Play();
+    }
+
+    public void PlaySoundShot()
+    {
+        audioSource_Shot.clip = shot;
+
+        audioSource_Shot.Play();
+    }
+}
