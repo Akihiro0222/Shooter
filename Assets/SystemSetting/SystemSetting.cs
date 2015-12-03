b84b197f9046fcc7fecd54216231e72df486f484
Akihiro0222 <ak08222@yahoo.co.jp>
2015-12-03 15:09:00 +0900

    解像度の固定化

---
+++ Assets/SystemSetting/SystemSetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemSetting : MonoBehaviour {
+
+	
+    void Awake()
+    {
+        int WindowWidth = 1100;
+        int WindowHeight = 1600;
+
+        bool FullscreenFlag = false;
+
+        int FreshRate = 60;
+
+        Screen.SetResolution(WindowWidth, WindowHeight, FullscreenFlag,FreshRate);
+    }
+	
+
+}
